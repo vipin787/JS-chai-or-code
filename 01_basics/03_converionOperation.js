e0c1e344c8cc9a152878490c0df405afb0fb08ae
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2024-01-06 06:55:35 +0000

    practicing of conversion operations

---
+++ 01_basics/03_converionOperation.js
@@ -0,0 +1,45 @@
+// let score = 33;
+// console.log(typeof score);
+
+// let Value = String(score)
+// console.log(typeof Value);
+
+// let age = undefined;
+// console.log(typeof age);
+// console.log(age);
+
+//  type of undefined will always be undefined
+
+// var valua2 = null;
+// console.log(typeof valua2);
+// console.log(valua2);
+
+// here value2 value will be null, but when check its data type of null it will be object and datatype of null
+
+// const value3 = NaN;
+// console.log(value3);
+// console.log(typeof value3);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// 1 => true , 0 => false    boolean type of 
+// "" => false, "vipin" => true
+
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(typeof +true);
+console.log(typeof -true);
+console.log(+true);
+// console.log(++true);
+console.log(-true);
+console.log(typeof String(+true));
+
+
+
+// "33" => 33 we can conver string into number using Strinf method
+
+//  "33abc" => NaN  it is because 33abc is really not a number but it can convert string and into number by using string and number methods
+// true => 1; false => 0;  
